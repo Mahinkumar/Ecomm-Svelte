f682cff6e8df4277cfc7fe72a8c37fdccb79c2a6
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-08-01 23:13:10 +0530

    Update configs

--- src/lib/config.js
+++ src/lib/config.js
@@ -9,4 +9,13 @@ export const icon_name = "Svelte Icon"
 export const icon_path = "Svelte_Logo.svg" 
 //Svg image types are recommended as they scale for all possible sizes
 //////////////////////////////////////////////////////////////////////
-
+// Add the details you want from customer for signup
+// NOTE: The only availible types are the ones supported by html input
+export const details = {
+    name: 'text',
+    email: 'email',
+    address: 'text',
+    is_seller: 'boolean',
+    phone_no: 'number'
+}
+///////////////////////////////////////////////////////////////////////
