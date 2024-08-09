ba099511aff65e7e3f6aa2fbe96aa2a5f668cbf8
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-08-09 16:28:55 +0530

    Setup for basic Layout

--- src/routes/[[app]]/+page.svelte
+++ src/routes/[[app]]/+page.svelte
@@ -1,8 +1,12 @@
-
-<div class="flex flex-col justify-center items-center h-screen">
-    <h1 class="text-5xl">Home Page</h1>
-    <form method="post">
-    <button class="bg-orange-500 p-2 mt-3 text-3xl" formaction="?/logout">Test Logout</button>
-    <button class="bg-orange-500 p-2 mt-3 text-3xl" formaction="?/logout_all">Test Logout All</button>
-    </form>
+<div class="w-screen min-h-screen dark:bg-black bg-white">
+    <nav class="h-12 bg-blue-500 justify-center items-center flex">Some Navigation</nav>
+    <div class="pb h-96 text-white w-screen grid grid-cols-4">
+        <!--Combine Multiple blocks for an Ad.-->
+        {#each [1,2,3,4,5,6,7,8] as i}
+            <div class="bg-green pb flex justify-center items-center text-xl">Block: {i}</div>
+        {/each}
+    </div>
+    <div class="flex pb h-72 justify-center items-center text-2xl text-white">
+        Section Goes Here
+    </div>
 </div>
